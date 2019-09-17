f853223cbe2add3727cf7b53ea0cbd438cbc7ef6
Adrian-Garcia <agarc5@hotmail.com>
2019-09-17 17:02:13 -0500

    created axiosAuth.js 2 fix axios 2 accept token

--- friends/src/component/Friends.js
+++ friends/src/component/Friends.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import {axiosWithAuth} from '../utils/axiosAuth'
+
+ function Friends(props){
+    const [list, setList] = useState([]);
+
+    // console.log(credentials, 'creds')
+const getData = () => {
+    axiosWithAuth()
+    .get('http://localhost:5000/api/friends', list)
+    .then(res=> {setList(res.data)})
 
 
- function Friends(){
+    .catch(err => console.log(err))
+    // console.log(props, 'props')
+}
+
+    getData();
+
     return (
+        <>
         <h1>Hello World</h1>
+        <div>
+        </div>
+        </>
     )
 }
 
